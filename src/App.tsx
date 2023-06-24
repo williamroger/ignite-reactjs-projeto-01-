1b2e533d21220bc7e6672fc17a5bbdb2ad10cb81
William Roger <williamroger.frontend@gmail.com>
2023-06-24 14:34:52 -0300

    extendendo interfaces e ajustes no componente Post

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Header } from './components/Header';
-import { Post } from './components/Post';
+import { Post, PostType } from './components/Post';
 import { Sidebar } from './components/Sidebar';
 
 import './global.css';
 import styles from './App.module.css';
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -67,9 +67,7 @@ function App() {
             return (
               <Post 
                 key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+                post={post}
               />
             )
           })}
